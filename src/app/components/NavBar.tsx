7c4f281c6b355289e8a27f32a3be15d18ee6ab09
ahmed-naeem99 <a10naeem@torontomu.ca>
2025-02-07 16:21:17 -0500

    Main Logo on navbar fixed

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,14 +12,13 @@ const poseyFont = localFont({
 const NavBar = async () => {
   const session = await getServerSession();
   return (
-    <div className="navbar dark:text-white dark:focus:text-white transition-colors  rounded-3xl">
-      {/* border-2 dark:border-gray-600/40 */}
+    <div className="navbar dark:text-white dark:focus:text-white transition-colors rounded-3xl">
       <div className="navbar-start">
         <div className="dropdown">
           <div
             tabIndex={0}
             role="button"
-            className="btn btn-ghost lg:hidden px-0 pr-3 sm:px-4 "
+            className="btn btn-ghost lg:hidden px-0 pr-3 sm:px-4"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -38,13 +37,8 @@ const NavBar = async () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52   bg-sky-800 text-white"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-sky-800 text-white"
           >
-            {/* <li>
-              <Link className="dark:focus:text-white" href="/login">
-                LoginTest
-              </Link>
-            </li> */}
             <li>
               <a>Missions</a>
               <ul className="p-2">
@@ -79,20 +73,15 @@ const NavBar = async () => {
           href="/"
           className="btn btn-ghost text-lg sm:text-3xl px-0 sm:px-4 font-normal"
         >
-          <div className={poseyFont.className}>MAC Reality Quest</div>
+          <div className={poseyFont.className}>Extreme Reality Quest</div>
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          {/* <li>
-            <Link className="dark:focus:text-white" href="/login">
-              LoginTest
-            </Link>
-          </li> */}
           <li>
             <details>
               <summary>Missions</summary>
-              <ul className="p-2  bg-sky-800">
+              <ul className="p-2 bg-sky-800">
                 <li>
                   <Link
                     className="dark:focus:text-white text-white"
@@ -147,13 +136,12 @@ const NavBar = async () => {
           </div>
           <ul
             tabIndex={0}
-            className=" mt-24 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52  bg-sky-800"
+            className="mt-24 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52 bg-sky-800"
           >
             {!session && (
               <li>
-                <Link className="text-white  justify-between" href="/login">
+                <Link className="text-white justify-between" href="/login">
                   Login
-                  {/* <span className="badge">New</span> */}
                 </Link>
               </li>
             )}
