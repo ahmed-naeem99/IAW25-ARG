4422338a7ab694b1ed993eeb0e2da1ccf93ad16b
ahmed-naeem99 <a10naeem@torontomu.ca>
2025-02-12 02:26:08 -0500

    Goofy refreshing problem fixed

--- src/app/components/HomeButton.tsx
+++ src/app/components/HomeButton.tsx
@@ -18,8 +18,8 @@ const HomeButton = () => {
     console.log("Session Status (Production):", session.status);
     console.log("User Data (Production):", session.data?.user);
   
-    if (session.status === "authenticated") {
-      if (session.data.user.mission !== -1) {
+    if (session.status == "authenticated") {
+      if (session.data?.user?.mission !== undefined || session.data.user.mission !== -1) {
         router.push(`/mission${session.data.user.mission}`);
       } else {
         router.push("/mission1");
